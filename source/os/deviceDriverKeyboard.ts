9a74f5b36fadfb05e4abb3d51b9411af17cf7d7d
cjb2k6 <cjb2k6@gmail.com>
2015-09-20 14:08:32 -0400

    CLI now accepts symbols and punctuation

--- source/os/deviceDriverKeyboard.ts
+++ source/os/deviceDriverKeyboard.ts
@@ -29,7 +29,7 @@ module TSOS {
             // Parse the params.    TODO: Check that the params are valid and osTrapError if not.
             var keyCode = params[0];
             var isShifted = params[1];
-            _Kernel.krnTrace("Key code:" + keyCode + " shifted:" + isShifted);
+            _Kernel.krnTrace("Key code:" + keyCode + " shifted:" + isShifted + " Symbol: " + String.fromCharCode(keyCode));
             var chr = "";
             // Check to see if we even want to deal with the key that was pressed.
             if (((keyCode >= 65) && (keyCode <= 90)) ||   // A..Z
@@ -43,10 +43,128 @@ module TSOS {
                 }
                 // TODO: Check for caps-lock and handle as shifted if so.
                 _KernelInputQueue.enqueue(chr);
-            } else if (((keyCode >= 48) && (keyCode <= 57)) ||   // digits
-                        (keyCode == 32)                     ||   // space
-                        (keyCode == 13)) {                       // enter
+            } else if ((keyCode == 32) ||   // space
+                (keyCode == 13)) {                        // enter
                 chr = String.fromCharCode(keyCode);
+                _KernelInputQueue.enqueue(chr);
+            } else if (((keyCode >= 48) && (keyCode <= 57))) { //digits
+                if (isShifted) {
+                    switch (keyCode){
+                        case 48:
+                            chr = ")";
+                            break;
+                        case 49:
+                            chr = "!";
+                            break;
+                        case 50:
+                            chr = "@";
+                            break;
+                        case 51:
+                            chr = "#";
+                            break;
+                        case 52:
+                            chr = "$";
+                            break;
+                        case 53:
+                            chr = "%";
+                            break;
+                        case 54:
+                            chr = "^";
+                            break;
+                        case 55:
+                            chr = "&";
+                            break;
+                        case 56:
+                            chr = "*";
+                            break;
+                        case 57:
+                            chr = "(";
+                            break;
+                    }
+                } else {
+                    chr = String.fromCharCode(keyCode);
+                }
+                _KernelInputQueue.enqueue(chr);
+            } else {
+                switch (keyCode) {
+                    case 186:
+                        if (isShifted) {
+                            chr = ":";
+                        } else {
+                            chr = ";";
+                        }
+                        break;
+                    case 187:
+                        if (isShifted) {
+                            chr = "+";
+                        } else {
+                            chr = "=";
+                        }
+                        break;
+                    case 188:
+                        if (isShifted) {
+                            chr = "<";
+                        } else {
+                            chr = ",";
+                        }
+                        break;
+                    case 189:
+                        if (isShifted) {
+                            chr = "_";
+                        } else {
+                            chr = "-";
+                        }
+                        break;
+                    case 190:
+                        if (isShifted) {
+                            chr = ">";
+                        } else {
+                            chr = ".";
+                        }
+                        break;
+                    case 191:
+                        if (isShifted) {
+                            chr = "?";
+                        } else {
+                            chr = "/";
+                        }
+                        break;
+                    case 192:
+                        if (isShifted) {
+                            chr = "~";
+                        } else {
+                            chr = "`";
+                        }
+                        break;
+                    case 219:
+                        if (isShifted) {
+                            chr = "{";
+                        } else {
+                            chr = "[";
+                        }
+                        break;
+                    case 220:
+                        if (isShifted) {
+                            chr = "|";
+                        } else {
+                            chr = "\\";
+                        }
+                        break;
+                    case 221:
+                        if (isShifted) {
+                            chr = "}";
+                        } else {
+                            chr = "]";
+                        }
+                        break;
+                    case 222:
+                        if (isShifted) {
+                            chr = "\"";
+                        } else {
+                            chr = "'";
+                        }
+                        break;
+                }
                 _KernelInputQueue.enqueue(chr);
             }
         }
